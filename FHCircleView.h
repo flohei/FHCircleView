f45f6f5e2548f12100d74df99e779758c92b2f5d
Florian Heiber <flo@flohei.de>
2013-02-27 11:18:12 +0100

    adding FHCircleView

---
+++ FHCircleView.h
@@ -0,0 +1,17 @@
+//
+//  FHCircleView.h
+//  CGTest
+//
+//  Created by Florian Heiber on 25.02.13.
+//  Copyright (c) 2013 rootof.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FHCircleView : UIView
+
+@property (strong, nonatomic) UIColor *color;
+
++ (FHCircleView *)new;
+
+@end
---
+++ FHCircleView.m
@@ -0,0 +1,48 @@
+//
+//  FHCircleView.m
+//  CGTest
+//
+//  Created by Florian Heiber on 25.02.13.
+//  Copyright (c) 2013 rootof.net. All rights reserved.
+//
+
+#import "FHCircleView.h"
+
+@implementation FHCircleView
+
+@synthesize color = _color;
+
++ (FHCircleView *)new {
+    return [[FHCircleView alloc] initWithFrame:CGRectMake(0, 0, 10, 10)];
+}
+
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self setBackgroundColor:[UIColor clearColor]];
+    }
+    return self;
+}
+
+- (void)setColor:(UIColor *)color {
+    _color = color;
+    [self setNeedsDisplay];
+}
+
+- (UIColor *)color {
+    if (!_color) {
+        return [UIColor lightGrayColor];
+    } else {
+        return _color;
+    }
+}
+
+- (void)drawRect:(CGRect)rect {
+    const CGFloat *components = CGColorGetComponents([[self color] CGColor]);
+    CGContextRef contextRef = UIGraphicsGetCurrentContext();
+    CGContextSetLineWidth(contextRef, 2.0);
+    CGContextSetFillColor(contextRef, components);
+    CGContextSetStrokeColor(contextRef, components);
+    CGContextFillEllipseInRect(contextRef, rect);
+}
+
+@end
